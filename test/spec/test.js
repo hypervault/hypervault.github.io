2cf8ef8b52b1996da791ab82376e78280a48d80b
Caleb Madrigal <caleb.madrigal@gmail.com>
2014-12-01 14:09:27 -0600

    Added tests to verify my understanding of how SJCL works.

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,6 +3,67 @@
 (function () {
   'use strict';
 
+  var plaintext = "Through gazing on the unquiet sky";
+  var password = "metempsychosis";
+  // Set mode to OCB2, keysize to 256 bits, and PBKDF2 iterations to 10000
+  var cryptoParams = { 'iter': 10000, 'mode': 'ocb2', 'ks': 256 };
+
+  describe('SJCL', function () {
+    describe('codec library', function () {
+      var correctBase64Msg = "VGhyb3VnaCBnYXppbmcgb24gdGhlIHVucXVpZXQgc2t5";
+      it('should convert from a UTF-8 string to Base64', function () {
+        var msgBits = sjcl.codec.utf8String.toBits(plaintext);
+        var base64Msg = sjcl.codec.base64.fromBits(msgBits);
+        expect(base64Msg).to.eql(correctBase64Msg);
+      });
+
+      it('should convert from an Base64 to a UTF-8 string', function () {
+        var msgBits = sjcl.codec.base64.toBits(correctBase64Msg);
+        var msg = sjcl.codec.utf8String.fromBits(msgBits)
+        expect(msg).to.eql(plaintext);
+      });
+    });
+  });
+
+  describe('SJCL', function () {
+    describe('encrypt function return value', function () {
+      var cipherData = sjcl.encrypt(password, plaintext, cryptoParams);
+      var parsedCipherData = JSON.parse(cipherData);
+
+      it('should contain the ciphertext, salt, and IV', function () {
+        expect(parsedCipherData).to.include.keys(['ct', 'iv', 'salt']);
+      });
+
+      it('should contain have mode set to OCB2', function () {
+        expect(parsedCipherData.mode).to.equal('ocb2');
+      });
+
+      it('should contain have key size set to 256 bits', function () {
+        expect(parsedCipherData.ks).to.equal(256);
+      });
+
+      it('should contain have PDKDF2 iteration number set to 10000', function () {
+        expect(parsedCipherData.iter).to.equal(10000);
+      });
+    });
+  });
+
+  describe('SJCL', function () {
+    describe('decrypt function', function () {
+      var cipherData = sjcl.encrypt(password, plaintext, cryptoParams);
+      var decryptedData = sjcl.decrypt(password, cipherData);
+
+      it('should return plaintext string', function () {
+        expect(decryptedData).to.equal(plaintext);
+      });
+
+      it('should throw a sjcl.exception.corrupt exception if the wrong password is used', function () {
+        expect(function () {sjcl.decrypt("wrong_password", cipherData)}).to.throw(sjcl.exception.corrupt);
+      });
+    });
+  });
+
+
   describe('Base64-Binary', function () {
     describe('base64 decoder', function () {
       it('should run here few assertions', function () {
@@ -10,30 +71,6 @@
         var expectedDecodedString = new Uint8Array([72, 121, 112, 101, 114, 118, 97, 117, 108, 116, 32, 70, 84, 87]);
         var decodedString = Base64Binary.decode(encodedString);
         expect(decodedString).to.eql(expectedDecodedString);
-      });
-    });
-  });
-
-  describe('SJCL', function () {
-    describe('codec library', function () {
-      it('should convert from a UTF-8 string to Base 64', function () {
-        var expectedBase64Msg = "VGhyb3VnaCBnYXppbmcgb24gdGhlIHVucXVpZXQgc2t5";
-        var msg = "Through gazing on the unquiet sky"
-        var msgBits = sjcl.codec.utf8String.toBits(msg);
-        var base64Msg = sjcl.codec.base64.fromBits(msgBits);
-        expect(base64Msg).to.eql(expectedBase64Msg);
-      });
-    });
-  });
-
-  describe('SJCL', function () {
-    describe('codec library', function () {
-      it('should convert from an Base 64 to a UTF-8 string', function () {
-        var expectedMsg = "Through gazing on the unquiet sky"
-        var base64Msg = "VGhyb3VnaCBnYXppbmcgb24gdGhlIHVucXVpZXQgc2t5";
-        var msgBits = sjcl.codec.base64.toBits(base64Msg);
-        var msg = sjcl.codec.utf8String.fromBits(msgBits)
-        expect(msg).to.eql(expectedMsg);
       });
     });
   });
