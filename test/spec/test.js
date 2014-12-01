8a65f30a9d3a70cdd9d1428d6feea6b347d860d2
Caleb Madrigal <caleb.madrigal@gmail.com>
2014-11-30 19:55:37 -0600

    Added SJCL and added some tests to verify my understanding of how it works. More pending.

--- test/spec/test.js
+++ test/spec/test.js
@@ -14,13 +14,26 @@
     });
   });
 
-  describe('CryptoJS', function () {
-    describe('base64 decoder', function () {
-      it('should decode base64 into a UInt8Array', function () {
-        var encodedString = "SHlwZXJ2YXVsdCBGVFc=";
-        var expectedDecodedString = new Uint8Array([72, 121, 112, 101, 114, 118, 97, 117, 108, 116, 32, 70, 84, 87]);
-        var decodedString = Base64Binary.decode(encodedString);
-        expect(decodedString).to.eql(expectedDecodedString);
+  describe('SJCL', function () {
+    describe('codec library', function () {
+      it('should convert from a UTF-8 string to Base 64', function () {
+        var expectedBase64Msg = "VGhyb3VnaCBnYXppbmcgb24gdGhlIHVucXVpZXQgc2t5";
+        var msg = "Through gazing on the unquiet sky"
+        var msgBits = sjcl.codec.utf8String.toBits(msg);
+        var base64Msg = sjcl.codec.base64.fromBits(msgBits);
+        expect(base64Msg).to.eql(expectedBase64Msg);
+      });
+    });
+  });
+
+  describe('SJCL', function () {
+    describe('codec library', function () {
+      it('should convert from an Base 64 to a UTF-8 string', function () {
+        var expectedMsg = "Through gazing on the unquiet sky"
+        var base64Msg = "VGhyb3VnaCBnYXppbmcgb24gdGhlIHVucXVpZXQgc2t5";
+        var msgBits = sjcl.codec.base64.toBits(base64Msg);
+        var msg = sjcl.codec.utf8String.fromBits(msgBits)
+        expect(msg).to.eql(expectedMsg);
       });
     });
   });
