698439761c1671c38340a3c729d3f433a9c9c84d
Caleb Madrigal <caleb.madrigal@gmail.com>
2014-12-20 00:11:24 -0600

    Got progress circle working for encryption

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,34 @@
+// Progress indicator
+
+// Set up SVG animation
+// see http://jakearchibald.com/2013/animated-line-drawing-svg/
+var progressPath = document.querySelector('.progress path');
+var pathLength = progressPath.getTotalLength();
+progressPath.style.transition = progressPath.style.WebkitTransition =
+  'none';
+progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+progressPath.style.strokeDashoffset = pathLength;
+progressPath.getBoundingClientRect();
+progressPath.style.transition = progressPath.style.WebkitTransition =
+  'stroke-dashoffset 300ms linear';
+
+var updateProgress = function (percent) {
+  var progress = pathLength - pathLength*(percent/100.0);
+  progressPath.style.strokeDashoffset = progress;
+  document.getElementById("percent").innerHTML = percent+"%";
+}
+
+// Progress hook
 var progress = [];
+
 var reset_progress = function () {
   progress = [];
   $("#progress-summary").html('');
 };
+
 var progress_hook = function(p) {
   var h, pr, _i, _len;
+  var progressPercent = 0;
   if (progress.length && (progress[progress.length - 1].what === p.what)) {
     progress[progress.length - 1] = p;
   } else {
@@ -14,10 +38,16 @@ var progress_hook = function(p) {
   for (_i = 0, _len = progress.length; _i < _len; _i++) {
     pr = progress[_i];
     h += "<li>" + pr.what + " " + pr.i + "/" + pr.total + "</li>";
+    if (_i == 0) {
+      progressPercent += (10.0 * (pr.i / pr.total));
+    }
+    else {
+      progressPercent += (15.0 * (pr.i / pr.total));
+    }
   }
+  updateProgress(Math.floor(progressPercent));
   return $("#progress-summary").html(h);
 };
-
 
 //////////////////////////////////////////////////////////////////////////////////// Read file stuff
 var globalFileData = {name:"", type:"", data:""};
