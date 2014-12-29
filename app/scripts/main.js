98b7bc1fd1cbc2d287e193b76278aa7ef6f7a22c
Caleb Madrigal <caleb.madrigal@gmail.com>
2014-12-28 20:29:07 -0600

    Unified main.js and vault.js, and removed vault.js. Also fixed 85% bug for decryption.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,9 @@
-// Progress indicator
+//////////////////////////////////////////////////////////////////////////// Encrypted embedded data
+var fileName = 'REPLACE_WITH_FILE_NAME_';
+var fileType = 'REPLACE_WITH_FILE_TYPE_';
+var cipherData = 'REPLACE_WITH_FILE_DATA_';
 
-// Set up SVG animation
-// see http://jakearchibald.com/2013/animated-line-drawing-svg/
+///////////////////////////////////////////////////////////////////////////////// Progress indicator
 var progressPath = document.querySelector('.progress path');
 var pathLength = progressPath.getTotalLength();
 progressPath.style.transition = progressPath.style.WebkitTransition =
@@ -26,7 +28,7 @@ var reset_progress = function () {
   document.getElementById("progress-summary").innerHTML = '';
 };
 
-var progress_hook = function(p) {
+var progress_hook_encrypt = function(p) {
   var h, pr, _i, _len;
   var progressPercent = 0;
   if (progress.length && (progress[progress.length - 1].what === p.what)) {
@@ -43,6 +45,29 @@ var progress_hook = function(p) {
     }
     else {
       progressPercent += (15.0 * (pr.i / pr.total));
+    }
+  }
+  updateProgress(Math.floor(progressPercent));
+  document.getElementById("progress-summary").innerHTML = h;
+};
+
+var progress_hook_decrypt = function(p) {
+  var h, pr, _i, _len;
+  var progressPercent = 0;
+  if (progress.length && (progress[progress.length - 1].what === p.what)) {
+    progress[progress.length - 1] = p;
+  } else {
+    progress.push(p);
+  }
+  h = "";
+  for (_i = 0, _len = progress.length; _i < _len; _i++) {
+    pr = progress[_i];
+    h += "<li>" + pr.what + " " + pr.i + "/" + pr.total + "</li>";
+    if (_i == 0) {
+      progressPercent += (10.0 * (pr.i / pr.total));
+    }
+    else {
+      progressPercent += (18.0 * (pr.i / pr.total));
     }
   }
   updateProgress(Math.floor(progressPercent));
@@ -82,29 +107,17 @@ function getFileData() {
 }
 
 // Drag and drop
-var dropzone = document.getElementById('dropzone')
-dropzone.ondragover = function () { this.className = 'hover'; return false; };
-dropzone.ondragend = function () { this.className = ''; return false; };
-dropzone.ondragleave = function () { this.className = ''; return false; };
-dropzone.ondrop = function (e) {
-  this.className = '';
-  e.preventDefault();
-  //readfiles(e.dataTransfer.files);
-}
-
-/////////////////////////////////////////////////////////////////////////////////// Encryption stuff
-function encryptFileData(fileData, password, callback) {
-  var data = new triplesec.Buffer(fileData);
-  var key = new triplesec.Buffer(password);
-
-  reset_progress();
-  triplesec.encrypt({key:key, progress_hook: progress_hook, data:data}, function (err, encryptedData) {
-    callback(err, encryptedData.toString('base64'));
-  });
-}
+// var dropzone = document.getElementById('dropzone')
+// dropzone.ondragover = function () { this.className = 'hover'; return false; };
+// dropzone.ondragend = function () { this.className = ''; return false; };
+// dropzone.ondragleave = function () { this.className = ''; return false; };
+// dropzone.ondrop = function (e) {
+//   this.className = '';
+//   e.preventDefault();
+//   //readfiles(e.dataTransfer.files);
+// }
 
 //////////////////////////////////////////////////////////////////////////////////// Vault rendering
-
 function getVaultTemplate(callback) {
   url = "vault.html"
   var xhr = new XMLHttpRequest();
@@ -125,9 +138,9 @@ function stripDataPrefix(dataUrl) {
 function renderVault(fileName, fileType, fileData, callback) {
   getVaultTemplate(function (vaultTemplate) {
     // Replace the 3 placeholders with the file name, file type, and file data
-    var vault = vaultTemplate.replace('REPLACE_WITH_FILE_NAME', fileName)
-      .replace('REPLACE_WITH_FILE_TYPE', fileType)
-      .replace('REPLACE_WITH_FILE_DATA', fileData);
+    var vault = vaultTemplate.replace('REPLACE_WITH_FILE_NAME' + String.fromCharCode(95), fileName)
+      .replace('REPLACE_WITH_FILE_TYPE' + String.fromCharCode(95), fileType)
+      .replace('REPLACE_WITH_FILE_DATA' + String.fromCharCode(95), fileData);
     callback(vault);
   });
 }
@@ -145,6 +158,39 @@ function createVault() {
     renderVault(globalFileData['name'], globalFileData['type'], encryptedData, function (vaultData) {
       saveVault(vaultData, "vault1.html");
     });
+  });
+}
+
+/////////////////////////////////////////////////////////////////////////////////// Encryption stuff
+function encryptFileData(fileData, password, callback) {
+  var data = new triplesec.Buffer(fileData);
+  var key = new triplesec.Buffer(password);
+
+  reset_progress();
+  triplesec.encrypt({key:key, progress_hook: progress_hook_encrypt, data:data}, function (err, encryptedData) {
+    callback(err, encryptedData.toString('base64'));
+  });
+}
+
+/////////////////////////////////////////////////////////////////////////////////// Decryption stuff
+function decryptFileData(cipherData, password, callback) {
+  var data = new triplesec.Buffer(cipherData, 'base64');
+  var key = new triplesec.Buffer(password);
+
+  reset_progress();
+  triplesec.decrypt({key:key, progress_hook: progress_hook_decrypt, data:data}, function (err, plainData) {
+    var plainDataBase64 = plainData.toString();     
+    callback(err, plainDataBase64);
+  });
+}
+
+function decryptAndDownload() {
+  var password = document.getElementById('password_input').value;
+  decryptFileData(cipherData, password, function (err, plainData) {
+    var blob = new Blob([Base64Binary.decode(plainData)], {
+        type: fileType
+    });
+    saveAs(blob, fileName);
   });
 }
 
